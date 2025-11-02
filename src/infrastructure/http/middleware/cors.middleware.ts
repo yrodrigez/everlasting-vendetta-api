c5ce52846f7e74879a533ae26f586f39d9fd15d6
Yago <yarlorenzo@gmail.com>
2025-11-02 22:39:12 +0100

    fix(cors): add www subdomain to allowed origins

--- src/infrastructure/http/middleware/cors.middleware.ts
+++ src/infrastructure/http/middleware/cors.middleware.ts
@@ -1,7 +1,12 @@
 import { cors } from "hono/cors";
 
 export const corsMiddleware = cors({
-    origin: ["https://everlastingvendetta.com", "http://localhost:3000", "https://everlasting-vendetta.vercel.app/"],
+    origin: [
+        "https://everlastingvendetta.com",
+        "https://www.everlastingvendetta.com",
+        "http://localhost:3000",
+        "https://everlasting-vendetta.vercel.app"
+    ],
     credentials: true,
     allowMethods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowHeaders: ["Content-Type", "Authorization", "X-Request-ID"],
