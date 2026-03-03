d31920ea9660332280efbc80d63f0a78ec3e6318
Yago <yarlorenzo@gmail.com>
2026-03-03 16:50:26 +0100

    fix(role-repo): added a fix to roles repository that didn't allow adding the role with user id

--- src/infrastructure/repositories/role-repository.ts
+++ src/infrastructure/repositories/role-repository.ts
@@ -10,26 +10,34 @@ export class RoleRepository implements IRoleRepository {
 
     async findByMemberId(userId: string): Promise<string[]> {
         this.logger.debug(`Fetching roles for user ID: ${userId}`);
-        const { data, error } = await this.database
-            .from("ev_member_role")
-            .select('role, ev_member!inner(user_id)')
-            .eq('ev_member.user_id', userId)
-            .returns<{ role: string }[]>();
+        const [{ data: memberRoles, error: errorMembers }, { data: userRoles, error: userError }] = await Promise.all([
+            await this.database
+                .from("ev_member_role")
+                .select('role, ev_member!inner(user_id)')
+                .eq('ev_member.user_id', userId)
+                .overrideTypes<{ role: string }[]>(),
+            await this.database.from("ev_member_role")
+                .select('role')
+                .eq('user_id', userId)
+                .overrideTypes<{ role: string }[]>(),
+        ])
 
-        if (error) {
+        if (errorMembers || userError) {
             this.logger.error(
                 `Error fetching roles for member ${userId}:`,
-                error,
+                errorMembers || userError,
             );
             throw new Error(
-                `Error fetching roles for member ${userId}: ${error.message}`,
+                `Error fetching roles for member ${userId}: ${errorMembers?.message || userError?.message}`,
             );
         }
 
-        if (!data) {
+        if (!memberRoles && !userRoles) {
             this.logger.warn(`No roles found for member ID: ${userId}`);
             return [];
         }
+
+        const data = [...(memberRoles || []), ...(userRoles || [])];
 
         this.logger.debug(
             `Found ${data.length} roles for member ID: ${userId}`,
